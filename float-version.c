1741ef310dc9cc6a521ce908a0702f79dc128ba9
jesselacroix394 <jesselacroix394@gmail.com>
2022-07-22 18:10:41 -0700

    Added reflector, rotator and transpose functions
    Functions still require testing

--- float-version.c
+++ float-version.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <float.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-const int IMAGE_HEIGHT = 240;
-const int IMAGE_WIDTH = 320;
+#define IMAGE_HEIGHT 240
+#define IMAGE_WIDTH 320
+#define N 3
+#define M 3
+
 int IMAGE_SIZE = 240*320;
 float jpegMatrix [240*320];
 
@@ -35,16 +40,40 @@ void getImage(char *imageName){
         }
         else{
             jpegMatrix[i] = cbinary;
-            printf("%d ", cbinary);
+            // printf("%d ", cbinary);
         }
     }
 
-    printf("Array first value: %f\n", jpegMatrix[0]);
-    printf("Array last value: %f\n", jpegMatrix[IMAGE_SIZE-1]);
+    printf("\nArray first value: %f\n", jpegMatrix[0]);
+    printf("\nArray last value: %f\n", jpegMatrix[IMAGE_SIZE-1]);
 
     while((cbinary = fgetc(imageFile)) != EOF){
         printf("%d ", cbinary);
     }
+}
+
+void transpose(float A[][N], float B[][M]){
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            B[i][j] = A[j][i];
+        }
+    }
+}
+
+void reflector(float I1, float I2, float O1, float O2){
+    O1 = I1 + I2;
+    O2 = I1 - I2;
+    printf("O1: %f, O2: %f\n", O1, O2);
+}
+
+void rotator(float I1, float I2, float k, float Cn, float O1, float O2){
+    O1 = ((k * sin((Cn * M_PI)/16)) - (k * cos((Cn * M_PI)/16)))*I2 + (k * cos((Cn * M_PI)/16)*(I1 + I2));
+    O2 = -((k * sin((Cn * M_PI)/16)) + (k * cos((Cn * M_PI)/16)))*I1 + (k * cos((Cn * M_PI)/16)*(I1 + I2));
+}
+
+void scaleUp(float I, float O){
+    O = sqrt(2) * I;
 }
 
 int main(int argc, char *argv[]){
@@ -57,6 +86,30 @@ int main(int argc, char *argv[]){
     getImage(argv[1]);
 
     printf("Success\n");
+
+    printf("\n\n----TESTING AREA----\n\n");
+
+    float *testMatrix;
+
+    float A[N][M] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+    float B[M][N];
+
+    transpose(A, B);
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
+
+    // float I1 = 1;
+    // float I2 = 2;
+    // float O1, O2;
+
+    // reflector(I1, I2, O1, O2);
+    // printf("I1: %f, I2: %f\n", I1, I2);
+    // printf("O1: %f, O2: %f\n", O1, O2);
 
     return 0;
 }
