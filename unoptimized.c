3b8b183d887777757f13066b93243d2a428283a5
brodygimson <brodygimson@uvic.ca>
2022-08-02 22:20:23 -0700

    Updated with Barr-C standard

--- unoptimized.c
+++ unoptimized.c
@@ -1,192 +1,236 @@
 #include <stdio.h>
 #include <string.h>
 #include <float.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
 #include <stdint.h>
-#include <float.h>
 
-#define IMAGE_HEIGHT 240
-#define IMAGE_WIDTH 320
-#define N 3
-#define M 3
+/*
+General Barr-C things that were followed included 
+- Rules under 7.1 for variables
+- Rules for bracing under 1.3
+- Rules for line widths under 1.2
+- Rules for parentheses under 1.4
+- White space rules under section 3
 
-int IMAGE_SIZE = 240*320;
-int32_t pixel_matrix[240][320];
-int32_t output_matrix[240][320];
-int32_t current_group[8][8];
-int32_t rotateConstO1[3] = {-12873, -4520, 12539};
-int32_t rotateConstO2[3] = {-19266, -22725, -30273};
-int32_t rotateConst[3] = {16069, 13622, 8866};
-int32_t sqrt2 = 181;
+More specific examples given as they appear and are assumed to be used throughout after
+*/
 
-void getImage(char *image_name){
-    FILE *image_file;
+// Image related constants
+int const IMAGE_SIZE = 240*320;
+int const IMAGE_HEIGHT = 240;
+int const IMAGE_WIDTH = 320;
+
+// Constants used in Loeffler's Algorithm
+int const SQRT2 = 181;
+int const ROTATE_CONST_p_output_1[3] = {-12873, -4520, 12539};      // Constants used for output 1 in rotators
+int const ROTATE_CONST_p_output_2[3] = {-19266, -22725, -30273};    // Constants used for output 2 in rotators
+int const ROTATE_CONST[3] = {16069, 13622, 8866};                   // Constants used for both in rotators
+int const END_SCALE = 16384;                                        // Ending scale factor
+
+// Barr-C: Global variables should start with "g_" (7.1.j)
+int32_t g_pixel_matrix[240][320];
+int32_t g_output_matrix[240][320];
+int32_t g_current_group[8][8];
+
+void getImage(char *p_image_name) 
+{
+    // Barr-C: Pointers should start with "_p" (7.1.k)
+    // Barr-C: variables initialized before use (7.2.a)
+    FILE *p_image_file;
     int cbinary;
     int error;
 
     fflush(stdout);
     
-    image_file = fopen(image_name, "rb");
-    if (image_file == NULL) {
+    p_image_file = fopen(p_image_name, "rb");
+
+    // Barr-C: When checking equivalence with a constant, the constant should be on the left (8.6.a)
+    if (NULL == p_image_file) 
+    {
         perror("Error: Could not open file");
     }
 
     //Move to end of file header
-    error = fseek(image_file, 54, SEEK_SET);
-    if (error != 0) {
+    error = fseek(p_image_file, 54, SEEK_SET);
+
+    if (0 != error) 
+    {
         perror("Error: Could not seek past header of file");
     }
     
     //Move to end of Colour table
-    error = fseek(image_file, 1024, SEEK_CUR);
-    if (error != 0) {
+    error = fseek(p_image_file, 1024, SEEK_CUR);
+
+    if (0 != error) 
+    {
         perror("Error: Could not seek past colour table of file");
     }
     
-    for(int i = 0; i < IMAGE_HEIGHT; i++){
-        for(int j = 0; j < IMAGE_WIDTH; j++) {
-            if((cbinary = fgetc(image_file)) == EOF){
+    for (int i = 0; i < IMAGE_HEIGHT; i++) 
+    {
+        for (int j = 0; j < IMAGE_WIDTH; j++) 
+        {
+            cbinary = fgetc(p_image_file);
+
+            if (cbinary  == EOF)
+            {
                 break;
             }
-            else{
-                pixel_matrix[i][j] = cbinary;
+            else
+            {
+                g_pixel_matrix[i][j] = cbinary;
             }
         }
     }
 }
 
-void transpose(int32_t A[][8], int32_t B[][8]){
-
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            B[i][j] = A[j][i];
+void transpose(int32_t orig[][8], int32_t trans[][8])
+{
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++) 
+        {
+            trans[i][j] = orig[j][i];
         }
     }
 }
 
-void getNextGroup(int current_x, int current_y) {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            current_group[i][j] = pixel_matrix[current_y + i][current_x + j];
+void get_next_group(int current_x, int current_y) 
+{
+    for (int i = 0; i < 8; i++) 
+    {
+        for (int j = 0; j < 8; j++) 
+        {
+            g_current_group[i][j] = g_pixel_matrix[current_y + i][current_x + j];
         }
     }
 }
 
-void * reflector(int32_t I1, int32_t I2, int32_t *O1, int32_t *O2){       
-    O1[0] = I1 + I2;
-    O2[0] = I1 - I2;
+void * reflector(int32_t input_1, int32_t input_2, int32_t *p_output_1, int32_t *p_output_2)
+{       
+    p_output_1[0] = input_1 + input_2;
+    p_output_2[0] = input_1 - input_2;
 }
 
-void rotator(int32_t I1, int32_t I2, int c, int32_t *O1, int32_t *O2){
-    O1[0] = (rotateConstO1[c])*I2 + (rotateConst[c])*(I1 + I2);
-    O2[0] = (rotateConstO2[c])*I1 + (rotateConst[c])*(I1 + I2);
+void rotator(int32_t input_1, int32_t input_2, int c, int32_t *p_output_1, int32_t *p_output_2)
+{
+    // Barr-C: Don't rely on C's operation precedence rules, use parentheses (1.4.a)
+    p_output_1[0] = (ROTATE_CONST_p_output_1[c] * input_2) + (ROTATE_CONST[c] * (input_1 + input_2));
+    p_output_2[0] = (ROTATE_CONST_p_output_2[c] * input_1) + (ROTATE_CONST[c] * (input_1 + input_2));
 }
 
-int32_t scaleUp(int32_t I){
-    I = I >> 7;
-    int32_t O = sqrt2 * I;
-    return O;
+int32_t scale_up(int32_t input)
+{
+    int32_t output;
+
+    input = input >> 7;
+    output = SQRT2 * input;
+    return output;
 }
 
-void * loefflers(int32_t * x){
-    
-    int32_t A[8];
+void * loefflers(int32_t * x)
+{
+    int32_t tmp_output[8];
     
     //stage 1
-    reflector(x[0], x[7], &A[0], &A[7]);
-    reflector(x[1], x[6], &A[1], &A[6]);
-    reflector(x[2], x[5], &A[2], &A[5]);
-    reflector(x[3], x[4], &A[3], &A[4]);
+    reflector(x[0], x[7], &tmp_output[0], &tmp_output[7]);
+    reflector(x[1], x[6], &tmp_output[1], &tmp_output[6]);
+    reflector(x[2], x[5], &tmp_output[2], &tmp_output[5]);
+    reflector(x[3], x[4], &tmp_output[3], &tmp_output[4]);
     
     //stage 2
-    reflector(A[0], A[3], &A[0], &A[3]);
-    reflector(A[1], A[2], &A[1], &A[2]);
+    reflector(tmp_output[0], tmp_output[3], &tmp_output[0], &tmp_output[3]);
+    reflector(tmp_output[1], tmp_output[2], &tmp_output[1], &tmp_output[2]);
     
-    rotator(A[4], A[7], 1, &A[4], &A[7]);
-    rotator(A[5], A[6], 0, &A[5], &A[6]);
+    rotator(tmp_output[4], tmp_output[7], 1, &tmp_output[4], &tmp_output[7]);
+    rotator(tmp_output[5], tmp_output[6], 0, &tmp_output[5], &tmp_output[6]);
     
     //stage 3
-    reflector(A[0], A[1], &A[0], &A[1]);
-    rotator(A[2], A[3], 2, &A[2], &A[3]);
-    reflector(A[4], A[6], &A[4], &A[6]);
-    reflector(A[7], A[5], &A[7], &A[5]);
-    
+    reflector(tmp_output[0], tmp_output[1], &tmp_output[0], &tmp_output[1]);
+    rotator(tmp_output[2], tmp_output[3], 2, &tmp_output[2], &tmp_output[3]);
+    reflector(tmp_output[4], tmp_output[6], &tmp_output[4], &tmp_output[6]);
+    reflector(tmp_output[7], tmp_output[5], &tmp_output[7], &tmp_output[5]);
     
     //stage 4
-    reflector(A[7], A[4], &A[7], &A[4]);
-    A[5] = scaleUp(A[5]);
-    A[6] = scaleUp(A[6]);
+    reflector(tmp_output[7], tmp_output[4], &tmp_output[7], &tmp_output[4]);
+    tmp_output[5] = scale_up(tmp_output[5]);
+    tmp_output[6] = scale_up(tmp_output[6]);  
     
-    
-    x[0] = A[0] << 7;
-    x[4] = A[1] << 7;
-    x[2] = A[2] >> 7;
-    x[6] = A[3] >> 7;
-    x[7] = A[4] >> 7;
-    x[3] = A[5] >> 7;
-    x[5] = A[6] >> 7;
-    x[1] = A[7] >> 7;
-    
+    // unscramble values
+    x[0] = tmp_output[0] << 7;
+    x[4] = tmp_output[1] << 7;
+    x[2] = tmp_output[2] >> 7;
+    x[6] = tmp_output[3] >> 7;
+    x[7] = tmp_output[4] >> 7;
+    x[3] = tmp_output[5] >> 7;
+    x[5] = tmp_output[6] >> 7;
+    x[1] = tmp_output[7] >> 7;
 }
 
-int main(int argc, char *argv[]){
-    int32_t current_group_t[8][8];
+int main(int argc, char *argv[])
+{
+    int32_t current_group_trans[8][8];
     
-    if (argc != 2){
+    if (argc != 2)
+    {
         printf("Error: 1 arg expected {filename}, received: %d\n", (argc-1));
         return 1;
     }
 
     printf("Filename: %s\n", argv[1]);
 
-    printf("\n----TESTING AREA----\n\n");
+    printf("\n----TESTING AREA----\n");
 
     getImage(argv[1]);
     
-    for(int x = 0; x < 40; x++){
-    	
-        for(int y = 0; y < 30; y++){
-    		getNextGroup(8*x, 8*y);
+    for (int x = 0; x < 40; x++)
+    {
+        for (int y = 0; y < 30; y++)
+        {
+    		get_next_group(8*x, 8*y);
     		
-    		for(int i = 0; i < 8; i++){
-    			loefflers(current_group[i]);
+    		for (int i = 0; i < 8; i++)
+            {
+    			loefflers(g_current_group[i]);
     		}	
     		
-    		transpose(current_group, current_group_t);
+    		transpose(g_current_group, current_group_trans);
     		
-    		for(int i = 0; i < 8; i++){
-    			loefflers(current_group_t[i]);
+    		for (int i = 0; i < 8; i++)
+            {
+    			loefflers(current_group_trans[i]);
     		}
     		
-    		transpose(current_group_t, current_group);
+    		transpose(current_group_trans, g_current_group);
 
-    		for (int i = 0; i < 8; i++) {
-
-        		for (int j = 0; j < 8; j++) {
-            			output_matrix[x*8 + i][y*8 + j] = current_group[i][j];
+    		for (int i = 0; i < 8; i++) 
+            {
+        		for (int j = 0; j < 8; j++) 
+                {
+            		g_output_matrix[x*8 + i][y*8 + j] = g_current_group[i][j];
         		}
     		}
     	}
     }
 
-    printf("Corner 8x8:\n");
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            printf("%f, ", output_matrix[i][j] / pow(2, 14));
+    printf("\nCorner 8x8:\n");
+    for (int i = 0; i < 8; i++) 
+    {
+        for (int j = 0; j < 8; j++) 
+        {
+            printf("%f, ", (double) g_output_matrix[i][j] / END_SCALE);
         }
         printf("\n");
     }
-    printf("\n");
 
-    printf("Center 8x8:\n");
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            printf("%f, ", output_matrix[i + 120][j + 160] / pow(2, 14));
+    printf("\nCenter 8x8:\n");
+    for (int i = 0; i < 8; i++) 
+    {
+        for (int j = 0; j < 8; j++) 
+        {
+            printf("%f, ", (double) g_output_matrix[i + 120][j + 160] / END_SCALE);
         }
         printf("\n");
     }
-    printf("\n");
     			
     return 0;
 }
